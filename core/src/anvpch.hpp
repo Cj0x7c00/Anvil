96257771343ca8387298b6f2f7e11df535f12319
Cayden <seawardspy1531@gmail.com>
2022-03-05 18:42:24 -0600

    added a precompiled header

---
+++ core/src/anvpch.hpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#define GLFW_INCLUDE_NONE
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <assert.h>
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+#include <cstring>
+#include <cstdlib>
+#include <optional>
+
+#include "simple_logger.hpp"
