2ddd2196cac355c21f2ede67b938ce26727bba88
Cayden <seawardspy1531@gmail.com>
2022-03-05 18:43:22 -0600

    made a settings file

---
+++ core/src/settings.hpp
@@ -0,0 +1,14 @@
+/**
+ * 
+ * This file will be used later on...
+ * 
+ */
+
+
+#pragma once
+
+#define VERSION 0
+#define DEBUG      // Mode ~NDEBUG
+
+//#define PATH_TO_VULKAN "../include/Vulkan/vulkan.h"
+//#define PATH_TO_GLFW "../include/GLFW/glfw3.h"
