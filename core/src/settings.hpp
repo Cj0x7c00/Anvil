337572341a17ce4f733a94ec24303a750ce23961
Cayden <seawardspy1531@gmail.com>
2022-03-16 17:28:20 -0500

    platform detection

--- core/src/settings.hpp
+++ core/src/settings.hpp
@@ -10,5 +10,12 @@
 #define VERSION 0
 #define DEBUG      // Mode ~NDEBUG
 
+#ifdef __APPLE__
+ #define PLATFORM_APPLE
+#endif
+#ifndef __APPLE__
+ #error "UNKNOWN PLATFORM"
+#endif
+
 //#define PATH_TO_VULKAN "../include/Vulkan/vulkan.h"
 //#define PATH_TO_GLFW "../include/GLFW/glfw3.h"
