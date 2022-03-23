7eeef0dccc5313c874ab3f1f87ee64172cd3d608
Cayden <seawardspy1531@gmail.com>
2022-03-23 07:58:36 -0500

    will be usedfor later use

--- core/src/anvRenderer.hpp
+++ core/src/anvRenderer.hpp
@@ -6,25 +6,11 @@ namespace AnvilEngine
 {
     class anvRenderer
     {
-    private:
-        /* data */
+
     public:
-        void Init(GLFWwindow* window);
+        void Init();
+
         void Clean();
-
-        auto IsDeviceSuitable(VkPhysicalDevice device);
-
-        VkSurfaceFormatKHR ChooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats);
-
-        VkPresentModeKHR ChooseSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes);
-
-        VkExtent2D ChooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities, GLFWwindow* window);
-
-        VK_OBJ::SwapChainSupportDetails QuerySwapChainSupport(VkPhysicalDevice device);
-
-        void CreateSurface(GLFWwindow* window);
-
-        void CreateSwapChain(GLFWwindow* window);
     };
     
     
