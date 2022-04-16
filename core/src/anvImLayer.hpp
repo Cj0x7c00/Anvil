cf40e1af37d1541d04e64ee0fbdda98883fead56
Cayden <seawardspy1531@gmail.com>
2022-04-16 01:45:49 -0500

    files that ill use later on, testing imgui

---
+++ core/src/anvImLayer.hpp
@@ -0,0 +1,33 @@
+#pragma once
+// Imgui UI
+/*
+#include "anvLog.hpp"
+#include "include/imgui/imgui.h"
+#include "include/imgui/backends/imgui_impl_glfw.h"
+#include "include/imgui/backends/imgui_impl_vulkan.h"
+
+#include "anvDevice.hpp"
+#include "anvSwapChain.hpp"
+
+namespace AnvilEngine{
+
+    class anvImLayer
+    {   
+        public:
+            anvImLayer(AnvDevice &m_device, GLFWwindow* m_window, anvSwapChain &sc);
+            ~anvImLayer();
+            void ImDraw(VkCommandBuffer cmdBuffer);
+
+        private:
+            ImGui_ImplVulkan_InitInfo initInfo{};
+            AnvDevice &device;
+            anvSwapChain &swapChain;
+            GLFWwindow* window;
+
+            void InitImGUI();
+            
+            void CheckVkResult(VkResult err);
+    };
+
+}
+*/
