8f6cc7fcd5e8d4a11bd75591180040bf64a14a3d
Cayden <seawardspy1531@gmail.com>
2022-06-22 22:26:14 -0500

    Abstraction && moved main func

--- core/src/app.cpp
+++ core/src/app.cpp
@@ -1,13 +1,231 @@
 #include "app.hpp"
-//#include "window_manager.hpp"    
 
-int main(){    
-    try {
-        AnvilEngine::AnvilEngineApplication app;
-        app.Run();
+#include "simpleRenderSystem.hpp"
+
+namespace AnvilEngine{
+
+        class GravityPhysicsSystem {
+        public:
+        GravityPhysicsSystem(float strength) : strengthGravity{strength} {}
+        RigidBody2dComponent rigidBody2d;
+        const float strengthGravity;
         
-    } 
-    catch (std::exception &e){
-        std::cout << e.what();
+        // dt stands for delta time, and specifies the amount of time to advance the simulation
+        // substeps is how many intervals to divide the forward time step in. More substeps result in a
+        // more stable simulation, but takes longer to compute
+        void update(std::vector<anvGameObject>& objs, float dt, unsigned int substeps = 1) {
+            const float stepDelta = dt / substeps;
+            for (int i = 0; i < substeps; i++) {
+            stepSimulation(objs, stepDelta);
+            }
+        }
+        
+        glm::vec2 computeForce(anvGameObject& fromObj, anvGameObject& toObj) const {
+            auto offset = fromObj.transform2d.translation - toObj.transform2d.translation;
+            float distanceSquared = glm::dot(offset, offset);
+        
+            // clown town - just going to return 0 if objects are too close together...
+            if (glm::abs(distanceSquared) < 1e-10f) {
+            return {.0f, .0f};
+            }
+        
+            float force =
+                strengthGravity * toObj.rigidBody2d.mass * fromObj.rigidBody2d.mass / distanceSquared;
+            return force * offset / glm::sqrt(distanceSquared);
+        }
+        
+        private:
+        void stepSimulation(std::vector<anvGameObject>& physicsObjs, float dt) {
+            // Loops through all pairs of objects and applies attractive force between them
+            for (auto iterA = physicsObjs.begin(); iterA != physicsObjs.end(); ++iterA) {
+            auto& objA = *iterA;
+            for (auto iterB = iterA; iterB != physicsObjs.end(); ++iterB) {
+                if (iterA == iterB) continue;
+                auto& objB = *iterB;
+        
+                auto force = computeForce(objA, objB);
+                objA.rigidBody2d.velocity += dt * -force / objA.rigidBody2d.mass;
+                objB.rigidBody2d.velocity += dt * force / objB.rigidBody2d.mass;
+            }
+            }
+        
+            // update each objects position based on its final velocity
+            for (auto& obj : physicsObjs) {
+            obj.transform2d.translation += dt * obj.rigidBody2d.velocity;
+            }
+        }
+    };
+    
+    class Vec2FieldSystem {
+        public:
+        void update(
+            const GravityPhysicsSystem& physicsSystem,
+            std::vector<anvGameObject>& physicsObjs,
+            std::vector<anvGameObject>& vectorField) {
+            // For each field line we caluclate the net graviation force for that point in space
+            for (auto& vf : vectorField) {
+            glm::vec2 direction{};
+            for (auto& obj : physicsObjs) {
+                direction += physicsSystem.computeForce(obj, vf);
+            }
+        
+            // This scales the length of the field line based on the log of the length
+            // values were chosen just through trial and error based on what i liked the look
+            // of and then the field line is rotated to point in the direction of the field
+            vf.transform2d.scale.x =
+                0.005f + 0.045f * glm::clamp(glm::log(glm::length(direction) + 1) / 3.f, 0.f, 1.f);
+            vf.transform2d.rotation = atan2(direction.y, direction.x);
+            }
+        }
+    };
+    
+    std::unique_ptr<anvModel> createSquareModel(AnvDevice& device, glm::vec2 offset) {
+        std::vector<anvModel::Vertex> vertices = {
+            {{-0.5f, -0.5f}},
+            {{0.5f, 0.5f}},
+            {{-0.5f, 0.5f}},
+            {{-0.5f, -0.5f}},
+            {{0.5f, -0.5f}},
+            {{0.5f, 0.5f}},  //
+        };
+        for (auto& v : vertices) {
+            v.position += offset;
+        }
+        return std::make_unique<anvModel>(device, vertices);
+        }
+        
+        std::unique_ptr<anvModel> createCircleModel(AnvDevice& device, unsigned int numSides) {
+        std::vector<anvModel::Vertex> uniqueVertices{};
+        for (int i = 0; i < numSides; i++) {
+            float angle = i * glm::two_pi<float>() / numSides;
+            uniqueVertices.push_back({{glm::cos(angle), glm::sin(angle)}});
+        }
+        uniqueVertices.push_back({});  // adds center vertex at 0, 0
+        
+        std::vector<anvModel::Vertex> vertices{};
+        for (int i = 0; i < numSides; i++) {
+            vertices.push_back(uniqueVertices[i]);
+            vertices.push_back(uniqueVertices[(i + 1) % numSides]);
+            vertices.push_back(uniqueVertices[numSides]);
+        }
+        return std::make_unique<anvModel>(device, vertices);
     }
+
+
+        AnvilEngineApplication::AnvilEngineApplication()
+        {
+            LoadGameObjects();
+            vkDeviceWaitIdle(anvDevice.m_device);
+        }
+
+        void AnvilEngineApplication::Run(){
+
+
+            
+            // create some models
+            std::shared_ptr<anvModel> squareModel = createSquareModel(
+                anvDevice,
+                {.5f, .0f});  // offset model by .5 so rotation occurs at edge rather than center of square
+            std::shared_ptr<anvModel> circleModel = createCircleModel(anvDevice, 64);
+            
+            // create physics objects
+            std::vector<anvGameObject> physicsObjects{};
+            auto red = anvGameObject::CreateGameObject();
+            red.transform2d.scale = glm::vec2{.05f};
+            red.transform2d.translation = {.5f, .5f};
+            red.color = {1.f, 0.f, 0.f};
+            red.rigidBody2d.velocity = {-.5f, .0f};
+            red.model = circleModel;
+            physicsObjects.push_back(std::move(red));
+            auto blue = anvGameObject::CreateGameObject();
+            blue.transform2d.scale = glm::vec2{.05f};
+            blue.transform2d.translation = {-.45f, -.25f};
+            blue.color = {0.f, 0.f, 1.f};
+            blue.rigidBody2d.velocity = {.5f, .0f};
+            blue.model = circleModel;
+            physicsObjects.push_back(std::move(blue));
+            
+            // create vector field
+            std::vector<anvGameObject> vectorField{};
+            int gridCount = 40;
+            for (int i = 0; i < gridCount; i++) {
+                for (int j = 0; j < gridCount; j++) {
+                auto vf = anvGameObject::CreateGameObject();
+                vf.transform2d.scale = glm::vec2(0.005f);
+                vf.transform2d.translation = {
+                    -1.0f + (i + 0.5f) * 2.0f / gridCount,
+                    -1.0f + (j + 0.5f) * 2.0f / gridCount};
+                vf.color = glm::vec3(1.0f);
+                vf.model = squareModel;
+                vectorField.push_back(std::move(vf));
+                }
+            }
+            
+            GravityPhysicsSystem gravitySystem{0.81f};
+            Vec2FieldSystem vecFieldSystem{};
+
+
+
+            SimpleRenderSystem sRenderSystem{anvDevice, AnvRenderer.getSwapChainRenderPass()};
+
+            while (!glfwWindowShouldClose(WindowManager.Window)){
+
+                glfwPollEvents();
+
+                if (auto commandBuffer = AnvRenderer.BeginFrame())
+                {
+                    // update systems
+                    gravitySystem.update(physicsObjects, 1.f / 60, 5);
+                    vecFieldSystem.update(gravitySystem, physicsObjects, vectorField);
+
+                    AnvRenderer.BeginSwapChainRenderPass(commandBuffer);
+                    sRenderSystem.RenderGameObjects(commandBuffer, physicsObjects);
+                    sRenderSystem.RenderGameObjects(commandBuffer, vectorField);
+                    AnvRenderer.EndSwapChainRenderPass(commandBuffer);
+                    AnvRenderer.EndFrame();
+                }
+                
+            }
+
+            vkDeviceWaitIdle(anvDevice.m_device);
+        }
+
+        void AnvilEngineApplication::LoadGameObjects()
+        {
+            std::vector<anvModel::Vertex> vertices{
+                /*{{position}, {color}}*/
+                /*TRIANGLE 1*/
+                {{0.0f, -0.5f},{1.0f, 0.0f, 0.0f}},
+                {{0.5f, 0.5f},{0.0f, 1.0f, 0.0f}},
+                {{-0.5f, 0.5f},{0.0f, 0.0f, 1.0f}}
+                
+            };
+
+            auto anv_model = std::make_shared<anvModel>(anvDevice, vertices);
+           
+            std::vector<glm::vec3> colors{
+                {1.f, .8f, .80f},
+                {1.f, .67f, .53f},
+                {1.f, .87f, .73f},
+                {.73f, 1.f, .8f},
+                {.73, .88f, 1.f}  
+            };
+            for (auto& color : colors) {
+                color = glm::pow(color, glm::vec3{2.2f});
+            }
+
+
+            for (int i = 0; i < 40; i++) {
+                auto triangle = anvGameObject::CreateGameObject();
+                triangle.model = anv_model;
+                triangle.transform2d.scale = glm::vec2(.5f) + i * 0.025f;
+                triangle.transform2d.rotation = i * glm::pi<float>() * .025f;
+                triangle.color = colors[i % colors.size()];
+                GameObjects.push_back(std::move(triangle));
+            }
+        }
+
 }
+
+
+
--- core/src/app.hpp
+++ core/src/app.hpp
@@ -1,13 +1,13 @@
 #pragma once
 #include "window_manager.hpp"
-#include "anvPipeline.hpp"
-#include "anvSwapChain.hpp"
-#include "anvModel.hpp"
+#include "anvGameObject.hpp"
+#include "anvRenderer.hpp"
 //#include "anvImLayer.hpp"
 
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/glm.hpp>
+#include <glm/gtc/constants.hpp>
 
 #include <array>
 #include <GLFW/glfw3.h>
@@ -15,11 +15,13 @@
 
 namespace AnvilEngine{
 
-    struct PushConstantData
-    {
-        glm::vec2 offset;
-        alignas(16) glm::vec3 color;
-    };
+
+
+
+
+
+    
+
 
     class AnvilEngineApplication{
 
@@ -27,245 +29,14 @@ namespace AnvilEngine{
             
             WindowManager WindowManager{800, 600, "Anvil Engine ver 0.0.0"};
             AnvDevice anvDevice{WindowManager.Window};
-            VkPipelineLayout pipelineLayout;
-            std::unique_ptr<anvPipeline> AnvilPipeline;
-            std::vector<VkCommandBuffer> CommandBuffers;
-            std::unique_ptr<anvSwapChain> AnvilSwapChain;
-            //anvImLayer ImLayer{anvDevice, WindowManager.Window, AnvilSwapChain};
-            std::unique_ptr<anvModel> model;
+            anvRenderer AnvRenderer{WindowManager, anvDevice};
+            std::vector<anvGameObject> GameObjects;
 
-            
-
-            AnvilEngineApplication()
-            {
-                ENGINE_INFO("Starting Engine");
-                ENGINE_INFO(glfwGetVersionString());
-                LoadModels();
-                CreatePipelineLayout();
-                RecreateSwapChain();
-                CreateCommandBuffers();
-            }
+            AnvilEngineApplication();   
 
 
-            anvSwapChain EngineGetSwapChain()
-            {
-                //return AnvilSwapChain;
-            }
-
-
-            void RecreateSwapChain()
-            {
-                auto Extent = WindowManager.GetExtent();
-
-                while (Extent.width == 0 || Extent.height == 0)
-                {
-                    Extent = WindowManager.GetExtent();
-                    glfwWaitEvents();
-                }
-
-                vkDeviceWaitIdle(anvDevice.m_device);
-
-                if (AnvilSwapChain == nullptr){
-                    AnvilSwapChain = std::make_unique<anvSwapChain>(anvDevice, Extent);
-                } else {
-                    AnvilSwapChain = std::make_unique<anvSwapChain>(anvDevice, Extent, std::move(AnvilSwapChain));
-                    if (AnvilSwapChain->imageCount() != CommandBuffers.size())
-                    {
-                        FreeCommandBuffers();
-                        CreateCommandBuffers();
-                    }
-                }
-                CreatePipeline();
-            }
-
-
-            void RecordCommandBuffer(int ImageIndex)
-            {
-                static int frame = 0;
-                frame = (frame + 1) % 1000;
-
-                VkCommandBufferBeginInfo beginInfo{};
-                beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
-                if (vkBeginCommandBuffer(CommandBuffers[ImageIndex], &beginInfo) != VK_SUCCESS)
-                {
-                    ENGINE_ERROR("Failed to begin recording command buffer");
-                }
-
-                VkRenderPassBeginInfo renderPassInfo{};
-                renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
-                renderPassInfo.renderPass = AnvilSwapChain->getRenderPass();
-                renderPassInfo.framebuffer = AnvilSwapChain->getFrameBuffer(ImageIndex);
-
-                renderPassInfo.renderArea.offset = {0, 0};
-                renderPassInfo.renderArea.extent = AnvilSwapChain->getSwapChainExtent();
-
-                std::array<VkClearValue, 2> clearValues{};
-                clearValues[0].color = {0.01f, 0.01f, 0.01f, 1.0f};
-                clearValues[1].depthStencil = {1.0f, 0};
-                renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
-                renderPassInfo.pClearValues = clearValues.data();
-
-                vkCmdBeginRenderPass(CommandBuffers[ImageIndex], &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
-
-                VkViewport viewport{};
-                viewport.x = 0.0f;
-                viewport.y = 0.0f;
-                viewport.width = static_cast<uint32_t>(AnvilSwapChain->getSwapChainExtent().width);
-                viewport.height = static_cast<uint32_t>(AnvilSwapChain->getSwapChainExtent().height);
-                viewport.minDepth = 0.0f;
-                viewport.maxDepth = 1.0f;
-                VkRect2D scissor{{0, 0}, AnvilSwapChain->getSwapChainExtent()};
-                vkCmdSetViewport(CommandBuffers[ImageIndex], 0, 1, &viewport);
-                vkCmdSetScissor(CommandBuffers[ImageIndex], 0, 1, &scissor);
-
-                AnvilPipeline->Bind(CommandBuffers[ImageIndex]);
-                model->Bind(CommandBuffers[ImageIndex]);
-
-                for (int j = 0; j < 4; j++)
-                {
-                    PushConstantData push{};
-                    push.offset = {-0.5f + frame * 0.002f, -0.4f + j * 0.25f};
-                    push.color = {0.0f, 0.0f, 0.2f + 0.2f * j};
-
-                    vkCmdPushConstants(
-                        CommandBuffers[ImageIndex], 
-                        pipelineLayout, 
-                        VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT, 
-                        0, 
-                        sizeof(PushConstantData), &push);
-                    model->Draw(CommandBuffers[ImageIndex]);
-                }
-
-                vkCmdEndRenderPass(CommandBuffers[ImageIndex]);
-                if (vkEndCommandBuffer(CommandBuffers[ImageIndex]) != VK_SUCCESS)
-                {
-                    ENGINE_ERROR("Failed to record command buffer");
-                }
-            }
-
-
-            void CreatePipelineLayout()
-            {
-                VkPushConstantRange pushConstantRange{};
-                pushConstantRange.stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT;
-                pushConstantRange.offset = 0;
-                pushConstantRange.size = sizeof(PushConstantData);
-
-                VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
-                pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
-                pipelineLayoutInfo.setLayoutCount = 0;
-                pipelineLayoutInfo.pSetLayouts = nullptr;
-                pipelineLayoutInfo.pushConstantRangeCount = 1;
-                pipelineLayoutInfo.pPushConstantRanges = &pushConstantRange;
-                if (vkCreatePipelineLayout(anvDevice.m_device, &pipelineLayoutInfo, nullptr, &pipelineLayout) != VK_SUCCESS){
-                    ENGINE_ERROR("Failed to create pipeline layout");
-                }
-            }
-
-
-
-            void CreatePipeline()
-            {
-
-                PipelineCfgInfo pipelineInfo{};
-                auto pipelineConfig = anvPipeline::DefaultPipelinecfgInfo(pipelineInfo);
-                pipelineConfig.renderPass = AnvilSwapChain->getRenderPass();
-                pipelineConfig.pipelineLayout = pipelineLayout;
-                AnvilPipeline = std::make_unique<anvPipeline>(
-                    anvDevice, 
-                    "shaders/vshader.spv", 
-                    "shaders/fshader.spv", 
-                    pipelineConfig);
-            }
-
-
-
-            void CreateCommandBuffers()
-            {
-                CommandBuffers.resize(AnvilSwapChain->imageCount());
-
-                VkCommandBufferAllocateInfo allocInfo{};
-                allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
-                allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
-                allocInfo.commandPool = anvDevice.commandPool;
-                allocInfo.commandBufferCount = static_cast<uint32_t>(CommandBuffers.size());
-
-                if (vkAllocateCommandBuffers(anvDevice.m_device, &allocInfo, CommandBuffers.data()) != VK_SUCCESS)
-                {
-                    ENGINE_ERROR("Failed to allocate command buffers");
-                }
-            }
-
-
-
-            void FreeCommandBuffers()
-            {
-                vkFreeCommandBuffers(anvDevice.m_device, anvDevice.commandPool, static_cast<float>(CommandBuffers.size()), CommandBuffers.data());
-                CommandBuffers.clear();
-            }
-
-
-            void DrawFrame()
-            {
-                uint32_t imgIndex;
-                auto result = AnvilSwapChain->acquireNextImage(&imgIndex);
-
-                if (result == VK_ERROR_OUT_OF_DATE_KHR)
-                {
-                    RecreateSwapChain();
-                }
-
-                if (result != VK_SUCCESS && result != VK_SUBOPTIMAL_KHR)
-                {
-                    ENGINE_ERROR("Failed to aquire swap chain image");
-                }
-                
-                //ImLayer.ImDraw(CommandBuffers[imgIndex]);
-                RecordCommandBuffer(imgIndex);
-                result = AnvilSwapChain->submitCommandBuffers(&CommandBuffers[imgIndex], &imgIndex);
-
-                if (result == VK_ERROR_OUT_OF_DATE_KHR || VK_SUBOPTIMAL_KHR || WindowManager.WasWindowResized())
-                {
-                    WindowManager.ResetWindowResizedFlag();
-                    RecreateSwapChain();
-                    return;
-                }
-                if (result != VK_SUCCESS)
-                {
-                    ENGINE_ERROR("Failed to present swap chain image");
-                }
-            }
-
-
-            void Run(){
-                ENGINE_INFO("Running. . .");
-                while (!glfwWindowShouldClose(WindowManager.Window)){
-
-                    //ImGui::Begin("Hello World!");
-            
-
-                    glfwPollEvents();
-                    DrawFrame();
-                }
-            }
-
-            
-
-
-            void LoadModels()
-            {
-                std::vector<anvModel::Vertex> vertices{
-                    /*{{position}, {color}}*/
-
-                    /*TRIANGLE 1*/
-                    {{0.0f, -0.5f},{1.0f, 0.0f, 0.0f}},
-                    {{0.5f, 0.5f},{0.0f, 1.0f, 0.0f}},
-                    {{-0.5f, 0.5f},{0.0f, 0.0f, 1.0f}}
-                    
-                };
-
-                model = std::make_unique<anvModel>(anvDevice, vertices);
-            }
+            void Run();
+            void LoadGameObjects();
 
     };
 
