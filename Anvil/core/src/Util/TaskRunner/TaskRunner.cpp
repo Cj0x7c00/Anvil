04fc9dcbe8f4924c882c3d8650a2d674d351971a
Seawardspy1531 222222222 <seawardspy1531@gmail.com>
2023-12-03 02:36:30 -0600

    added command line interface utility to run tasks

---
+++ Anvil/core/src/Util/TaskRunner/TaskRunner.cpp
@@ -0,0 +1,75 @@
+#include "TaskRunner.h"
+
+#include <iostream>
+#include <filesystem>
+#include <fstream>
+#include <cstdio>
+#include "../anvLog.hpp"
+
+
+namespace Anvil
+{
+    std::string TaskRunner::Execute(const char* cmd, const char* filename)
+    {
+        std::string result;
+
+        // Execute the command and redirect its output to a file
+        std::string commandWithRedirect = std::string(cmd) + " > " + filename;
+        int exitCode = system(commandWithRedirect.c_str());
+
+        std::ifstream outputFile{ filename };
+
+        if (exitCode != 0) {
+            ENGINE_WARN("Command execution failed: {}", exitCode);
+        }
+
+        if (!outputFile) {
+            ENGINE_WARN("Failed to open the output file: {}", filename);
+            return result; // Return an empty result if file couldn't be opened
+        }
+
+        std::string line;
+        while (std::getline(outputFile, line)) {
+            result += line + '\n';
+        }
+
+        outputFile.close(); // Close the file explicitly
+
+        // Clean up: remove the output file
+        if (remove(filename) != 0) {
+            ENGINE_WARN("Failed to remove the output file.");
+        }
+
+        return result;
+    }
+
+    int TaskRunner::SilentExecute(const char* cmd, CallbackFunction callback, const char* filename)
+    {
+
+        // Execute the command and redirect its output to a file
+        std::string commandWithRedirect = std::string(cmd) + " > " + filename;
+        int exitCode = system(commandWithRedirect.c_str());
+
+        if (exitCode != 0)
+        {
+            std::ifstream file(filename);
+            std::string contents;
+
+            std::string line;
+            while (std::getline(file, line))
+            {
+                contents += line;
+            }
+
+            callback(contents);
+
+            file.close();
+        }
+
+        if (remove(filename) != 0) {
+            ENGINE_WARN("Failed to remove the output file.");
+        }
+
+        return exitCode;
+    }
+}
---
+++ Anvil/core/src/Util/TaskRunner/TaskRunner.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "../../settings.hpp"
+#include <string>
+
+namespace Anvil
+{
+
+	class TaskRunner
+	{
+
+	public:
+		typedef void (*CallbackFunction)(std::string);
+
+		static std::string Execute(const char* cmd, const char* filename = "cmd_output.txt");
+
+		/// <summary>
+		/// Silently execute a command.
+		/// </summary>
+		/// <param name="cmd">command</param>
+		/// <param name="callback">Callback function, hase to take in a string, calls if failed</param>
+		/// <param name="filename">intermediate file</param>
+		/// <returns>exit code, 0 if successful</returns>
+		static int SilentExecute(const char* cmd, CallbackFunction callback = NULL, const char* filename = "cmd_output.txt");
+	};
+
+}
