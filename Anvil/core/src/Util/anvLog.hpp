ff15f8f2ce936aa99183c06bcf7507404cfdffd7
Seawardspy1531 222222222 <seawardspy1531@gmail.com>
2023-12-03 02:38:20 -0600

    updated logging to now format strings

--- Anvil/core/src/Util/anvLog.hpp
+++ Anvil/core/src/Util/anvLog.hpp
@@ -1,73 +1,103 @@
 #pragma once
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <string>
+#include <mutex>
+#include <filesystem> // Include the filesystem library for file operations
+
+namespace Anvil
+{
 
 
-class logger{
-
+    class Log {
     public:
-        /* 
-        LOG  instructions: ("str of what you want to say", "location", <logging code>) 
-
-        logging codes:
-            0-info 
-            1-debug
-            2-warning
-            3-error 
-        */
-
-       // TODO: Add Special Info log
-        static void LOG(std::string log_msg, std::string log_location, int log_code){
-
-            if (log_code == 0){ // 0 is code for info
-                std::cout << "\033[1;30m[INFO] \033[0m" << "\033[1;30m[" << "Location: " << log_location << "]: \033[0m"<< log_msg << '\n';
-            }
-
-            if (log_code == 1){ // 1 is code for debug
-                std::cout << "\033[1;32m[DEBUG] \033[0m" << "\033[1;30m[" << "Location: " << log_location << "]: \033[0m" << log_msg << '\n';
-            }
-
-            if (log_code == 2){ // 2 is code for warning
-                std::cout << "\033[1;33m[WARNING] \033[0m" << "\033[1;30m[" << "Location: " << log_location << "]: \033[0m" << log_msg << '\n';
-            }
-
-            if (log_code == 3){ //error
-                std::cout <<"\033[1;31m[ERROR] \033[0m" << "\033[1;30m[" << "Location: " << log_location << "]: \033[0m\n";
-                throw std::runtime_error(log_msg);
-            }
-
+        // Static log function to format and print log messages
+        template <typename... Args>
+        static void log(std::string func, const std::string& format, Args... args) {
+            std::string message = formatString(format, args...);
+            std::cout << "\033[1;30m[INFO : " << func << "()]: \033[0m" << message << std::endl;
         }
 
-};
+        template <typename... Args>
+        static void debug(std::string func, const std::string& format, Args... args) {
+            std::string message = formatString(format, args...);
+            std::cout << "\033[1;32m[DBUG : " << func << "()]: \033[0m" << message << std::endl;
+        }
+
+        template <typename... Args>
+        static void warn(std::string func, const std::string& format, Args... args) {
+            std::string message = formatString(format, args...);
+            std::cout << "\033[1;33m[WARN : " << func << "()]: \033[0m" << message << std::endl;
+        }
+
+        template <typename... Args>
+        static void error(std::string func, const std::string& format, Args... args) {
+            std::string message = formatString(format, args...);
+            std::cout << "\033[1;31m[EROR : " << func << "()]: \033[0m" << message << std::endl;
+            std::abort();
+        }
+
+    private:
+        // Helper function to format the message
+        template <typename... Args>
+        static std::string formatString(const std::string& format, Args... args) {
+            std::stringstream ss;
+            formatStringInternal(ss, format, args...);
+            return ss.str();
+        }
+
+        static std::string formatString(const std::string& format) {
+            return format;
+        }
+
+        template <typename T>
+        static void formatStringInternal(std::stringstream& ss, const std::string& format) {
+            ss << format;
+        }
+
+        // Helper function to recursively format the message
+        template <typename T>
+        static void formatStringInternal(std::stringstream& ss, const std::string& format, T value) {
+            size_t pos = format.find("{}");
+            if (pos != std::string::npos) {
+                ss << format.substr(0, pos) << value;
+                formatStringInternal(ss, format.substr(pos + 2), value);
+            }
+            else {
+                ss << format;
+            }
+        }
 
 
-#if defined(DEBUG)
-#define ENGINE_INFO(...)  logger::LOG(__VA_ARGS__, 0)
-#define ENGINE_DEBUG(...) logger::LOG(__VA_ARGS__, 1)
-#define ENGINE_WARN(...)  logger::LOG(__VA_ARGS__, 2)
-#define ENGINE_ERROR(...) logger::LOG(__VA_ARGS__, 3)
+        template <typename T, typename... Args>
+        static void formatStringInternal(std::stringstream& ss, const std::string& format, T value, Args... args) {
+            size_t pos = format.find("{}");
+            if (pos != std::string::npos) {
+                ss << format.substr(0, pos) << value;
+                formatStringInternal(ss, format.substr(pos + 2), args...);
+            }
+            else {
+                ss << format;
+            }
+        }
+    };
+
+#define ENGINE_INFO(...)  Log::log  ( __FUNCTION__, __VA_ARGS__ )
+#define ENGINE_DEBUG(...) Log::debug( __FUNCTION__, __VA_ARGS__ )
+#define ENGINE_WARN(...)  Log::warn ( __FUNCTION__, __VA_ARGS__ )  
+#define ENGINE_ERROR(...) Log::error( __FUNCTION__, __VA_ARGS__ )
 
 
-#ifdef NDEBUG
-#define ENGINE_ASSERT(condition_and_message) ((void)0)
-#else
 #define ENGINE_ASSERT(condition_and_message) \
     do { \
         if (!(condition_and_message)) { \
             std::cerr << "Assertion failed: " << #condition_and_message << " (" << __FILE__ << ":" << __LINE__ << ")" << std::endl; \
             std::abort(); \
         } \
-    } while (false)
-#endif
+    } while (false);
 
+#define ENGINE_OUT(message) logger::LOG(message, " ", 4)
 
-#elif defined(RELEASE) 
-
-#define ENGINE_INFO(...)  
-#define ENGINE_DEBUG(...) 
-#define ENGINE_WARN(...)  
-#define ENGINE_ERROR(...)
-#define ENGINE_ASSERT(e)
-
-#endif
+}
