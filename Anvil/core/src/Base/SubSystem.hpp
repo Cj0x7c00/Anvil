b5e7df80a39b607c58d49a9eeb19c22691ea6f36
Cayden Jordan <seawardspy1531@gmail.com>
2024-01-14 23:57:24 -0600

    experiment, messing arround with subsystems

---
+++ Anvil/core/src/Base/SubSystem.hpp
@@ -0,0 +1,45 @@
+#pragma once
+#include "Pointer.hpp"
+#include ""
+
+namespace Anvil
+{
+
+	class AppSubSystem
+	{
+		virtual void Init() = 0;
+		virtual void Update() = 0;
+
+		virtual void OnEvent() = 0;
+	};
+
+    class EngineSubSystem {
+    public:
+        // Function to get the singleton instance
+        static Ref<EngineSubSystem> getInstance() {
+            // Create the instance if it doesn't exist
+            if (!instance) {
+                instance = Ref<EngineSubSystem>(new EngineSubSystem);
+            }
+            return instance;
+        }
+
+        // Your other member functions go here
+
+    private:
+        // Private constructor to prevent instantiation
+        EngineSubSystem(const char* name) {
+            ENGINE_INFO("Initializing: {}", name);
+        }
+
+        // Private destructor to prevent deletion
+        ~EngineSubSystem() {
+            // Cleanup if necessary
+        }
+
+        // Declare other private members as needed
+
+        // The singleton instance
+        static Ref<EngineSubSystem> instance;
+    };
+}
