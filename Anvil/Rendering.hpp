7b00c9e06203a7f22664776ad037fea55c9e03ec
Cayden Jordan <seawardspy1531@gmail.com>
2024-01-22 20:17:03 -0600

    all shader and pipeline creation handled by the factory class

---
+++ Anvil/Rendering.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "core/src/Renderer/Renderer.h"
+#include "core/src/Renderer/GrComp/GraphicsFactory.h" // Shaders, Pipelines
+
+
+typedef Anvil::Ref<Anvil::Shader>	SHADER;
+typedef Anvil::Ref<Anvil::Pipeline> PIPELINE;
