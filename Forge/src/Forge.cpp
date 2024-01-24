4fbff5445a8ce6fcb9f103d26f94527dc0a996f8
Cayden Jordan <seawardspy1531@gmail.com>
2024-01-24 15:05:09 -0600

    rendering

--- Forge/src/Forge.cpp
+++ Forge/src/Forge.cpp
@@ -7,9 +7,14 @@
 class Forge : public Anvil::AnvilApplication
 {
 public:
-	Forge()
+	Forge(Anvil::AppProperties _p) : AnvilApplication(_p)
 	{
 		PushLayer(new ForgeLayer());
+	}
+
+	~Forge()
+	{
+		
 	}
 
 	void Run() override
@@ -17,12 +22,22 @@ public:
         while (!GetWindow()->ShouldClose()) {
 
             GetWindow()->PollEvents();
+			Anvil::Renderer::NewFrame();
         }
+		Anvil::Renderer::WaitIdle();
 	}
 };
 
 // Entry point
 Anvil::AnvilApplication* Anvil::CreateApplication()
 {
-	return new Forge();
+	AppProperties props;
+	props.win_props.name = "Forge Editor";
+	props.win_props.width = 1400;
+	props.win_props.height = 750;
+
+	ANVIL_DEBUG("Work Dir: {}", props.wrkdir);
+	return new Forge(props);
 }
+
+
--- Forge/src/ForgeLayer.cpp
+++ Forge/src/ForgeLayer.cpp
@@ -3,20 +3,20 @@
 ForgeLayer::ForgeLayer() 
 	: AnvilLayer("Forge Editor Layer")
 {
-
+	ANVIL_ASSERT(true && "Assert msg");
 }
 
 void ForgeLayer::Attach()
 {
-
+	Anvil::ANVIL_INFO("Attach");
 }
 
 void ForgeLayer::Update()
 {
-
+	Anvil::ANVIL_INFO("Update");
 }
 
 void ForgeLayer::Detach()
 {
-
+	Anvil::ANVIL_INFO("Detach");
 }
