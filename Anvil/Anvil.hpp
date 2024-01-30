adc6b7059d33026a4ef6a70ff68ec4e8022d3290
Cayden Jordan <seawardspy1531@gmail.com>
2024-01-30 14:28:19 -0600

    just one include file

--- Anvil/Anvil.hpp
+++ Anvil/Anvil.hpp
@@ -1,9 +1,20 @@
 #pragma once
 
-#include "core/src/Util/Time/anvTimeStep.hpp"
 #include "core/src/Base/anvApplication.hpp"
-#include "core/src/Util/anvLog.hpp"
 #include "core/src/Layer/anvLayer.hpp"
+#include "core/src/Scene/Scene.h"
+#include "core/src/Scene/Entity.h"
+#include "core/src/Util/Time/anvTimeStep.hpp"
+#include "core/src/Util/anvLog.hpp"
 
-#include "core/src/UUID/uuid.h"
+/*=== RENDERING ===*/
+#include "core/src/Renderer/Renderer.h"
+#include "core/src/Renderer/GrComp/GraphicsFactory.h" // Shaders, Pipelines
+
+typedef Anvil::Ref<Anvil::Scene>    SCENE;
+typedef Anvil::Entity               ENTITY;
+
+typedef Anvil::Ref<Anvil::Shader>	SHADER;
+typedef Anvil::Ref<Anvil::Pipeline> PIPELINE;
+
 
--- Anvil/Rendering.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "core/src/Renderer/Renderer.h"
-#include "core/src/Renderer/GrComp/GraphicsFactory.h" // Shaders, Pipelines
-
-
-typedef Anvil::Ref<Anvil::Shader>	SHADER;
-typedef Anvil::Ref<Anvil::Pipeline> PIPELINE;
