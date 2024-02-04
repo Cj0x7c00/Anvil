ddf9b63c4d607565bfa83e422ade5ab95876d945
Cayden Jordan <seawardspy1531@gmail.com>
2024-02-04 14:48:09 -0600

    Auto binds the pipeline

--- Anvil/core/src/Renderer/CommandBuffer.h
+++ Anvil/core/src/Renderer/CommandBuffer.h
@@ -1,11 +1,17 @@
 #pragma once
 
 #include "../Base/Pointer.hpp"
+#include "Renderer.h"
 
 typedef struct VkCommandBuffer_T* VkCommandBuffer;
 
 namespace Anvil
 {
+	class Pipeline;
+	class RenderPass;
+
+	struct NewFrameInfo;
+
 	class CommandBuffer
 	{
 	public:
@@ -18,7 +24,9 @@ namespace Anvil
 
 		VkCommandBuffer& Get();
 
+		void BeginRecording(NewFrameInfo& _fi, Ref<Pipeline> _pipeline);
 		void BeginRecording();
+		void EndRecording(NewFrameInfo& _fi);
 		void EndRecording();
 		void Reset();
 
