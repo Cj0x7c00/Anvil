eac5d07bcb2e12a20316ec118e9a6560308b7877
Cayden Jordan <seawardspy1531@gmail.com>
2024-02-04 14:49:13 -0600

    Multiple Render Sytem Support (Still Working on it)

--- Anvil/core/src/Renderer/Renderer.h
+++ Anvil/core/src/Renderer/Renderer.h
@@ -8,29 +8,55 @@
 
 #pragma once
 #include "RenderPass.h"
-#include "RenderSystem.h"
+#include "GrComp/GraphicsFactory.h"
 #include "../Base/macros.hpp"
 #include "../Base/Pointer.hpp"
 #include "../Window/Window.hpp"
 #include "../Scene/SceneManager.h"
-#include "GrComp/GraphicsFactory.h"
+
+#define GLM_FORCE_RADIANS
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <chrono>
 
 typedef struct VkRenderPass_T* VkRenderPass;
 typedef struct VkCommandBuffer_T* VkCommandBuffer;
 typedef struct VkSemaphore_T* VkSemaphore;
 typedef struct VkFence_T* VkFence;
+
 enum VkResult;
 
 namespace Anvil
 {
 	class Devices;
 	class SwapChain;
+	class RenderSystem;
+	class CommandBuffer;
 
 	struct NewFrameInfo
 	{
 		uint32_t FrameIndex;
 		uint32_t ImageIndex;
-		Ref<RenderPass>	 RenderPass;
+
+		Ref<Scene> Scene;
+
+		Ref<CommandBuffer> CommandBuffer;
+		Ref<RenderPass>	   RenderPass;
+	};
+
+	struct ViewportInfo
+	{
+		float x;
+		float y;
+		float width;
+		float height;
+		float minDepth;
+		float maxDepth;
+
+		float scissorOffset[2]{0.f, 0.f};
+		Extent2D scissorExtent;
+
+		static ViewportInfo Default();
 	};
 
 	class ANV_API Renderer
@@ -39,12 +65,19 @@ namespace Anvil
 		static void Init(Ref<Window> window, SceneManager* _scene_manager);
 		static void UseSystem(Ref<RenderSystem> _system);
 		static void UseDefaultConfiguration();
+		static void BeginOneTimeOps();
 		static void NewFrame();
 		static void WaitIdle();
+		static void SetViewport(ViewportInfo& info, CommandBuffer* cmdBuff);
+		static void Submit(NewFrameInfo& _fi);
+		static void SubmitOneTimeCommands(CommandBuffer cmdBuffer);
 		static void CreateNewSwapChain();
 		static void WindowWasResized();
 
 		static Ref<RenderPass> GetRenderPass() { return m_FrameInfo.RenderPass; };
+		static Ref<SwapChain>  GetSwapChain();
+		static Ref<Window>     GetWindow();
+
 
 	public:
 		static const int MAX_FRAMES_IN_FLIGHT = 3;
@@ -52,16 +85,18 @@ namespace Anvil
 	private:
 		static void create_render_pass();
 		static void check_swapchain_suitability(VkResult res);
+
+
 		static void sync();
-		static void submit(uint32_t imgIndex);
 
 	private:
 		static NewFrameInfo m_FrameInfo;
 
+		static Ref<Window>		 m_Window;
 		static Ref<Devices>      m_Devices;
 		static Ref<SwapChain>    m_SwapChain;
-		static Ref<Window>		 m_Window;
-		static Ref<RenderSystem> m_RenderSystem;
+		static std::vector<Ref<RenderSystem>> m_RenderSystems;
+		static std::vector<Ref<CommandBuffer>> m_CommandBuffers;
 
 		static SceneManager* m_SceneManager;
 
