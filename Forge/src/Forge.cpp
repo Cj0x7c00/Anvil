9683c91084d0868b5465b40aa3e6e703624676e7
Cayden Jordan <seawardspy1531@gmail.com>
2024-02-05 19:42:49 -0600

    canvas testing

--- Forge/src/Forge.cpp
+++ Forge/src/Forge.cpp
@@ -1,11 +1,20 @@
 #include "Forge.h"
+#include "imgui/imgui.h"
 #include <core/src/Scene/Components.h>
+
+
 
 void Forge::Awake() 
 {
+
 	Scene = m_SceneManager.GetActiveScene();
+	auto Canvas = Scene->CreateCanvas();
+
+	Canvas->AddButton("Click Me!", { 0, 0 }, { 20, 40 }, []() { ANVIL_INFO("Clicked!"); });
 
 	Triangle1 = Scene->CreateEntity("Triangle");
+
+
 
 	const std::vector<Anvil::vertex> vertices = {
 		{{-0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}},
@@ -15,7 +24,7 @@ void Forge::Awake()
 	};
 
 	const std::vector<uint16_t> indices = {
-	0, 1, 2, 2, 3, 0
+		0, 1, 2, 2, 3, 0
 	};
 
 	auto Sprite = Triangle1->AddComponent<Anvil::SpriteComponent>(vertices, indices);
