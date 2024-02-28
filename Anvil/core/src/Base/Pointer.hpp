79a0bdfa14e1c4fead80c35cacd4a0596d5f2e42
Cayden Jordan <seawardspy1531@gmail.com>
2024-02-28 00:32:10 -0600

    docs

--- Anvil/core/src/Base/Pointer.hpp
+++ Anvil/core/src/Base/Pointer.hpp
@@ -6,14 +6,31 @@ namespace Anvil
 {
 	template<typename T>
 	using Scope = std::unique_ptr<T>;
+
+	/// <summary>
+	/// Unique ptr
+	/// </summary>
+	/// <typeparam name="T">Object</typeparam>
+	/// <typeparam name="...Args">Object Args</typeparam>
+	/// <param name="...args"></param>
+	/// <returns>Unique ptr of object</returns>
 	template<typename T, typename ... Args>
 	constexpr Scope<T> CreateScope(Args&& ... args)
 	{
 		return std::make_unique<T>(std::forward<Args>(args)...);
 	}
 
+
 	template<typename T>
 	using Ref = std::shared_ptr<T>;
+
+	/// <summary>
+	/// Shared ptr
+	/// </summary>
+	/// <typeparam name="T">Object</typeparam>
+	/// <typeparam name="...Args">Object Args</typeparam>
+	/// <param name="...args"></param>
+	/// <returns>Shared ptr of object</returns>
 	template<typename T, typename ... Args>
 	constexpr Ref<T> CreateRef(Args&& ... args)
 	{
