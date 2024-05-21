1605346f3c665494f3fc8a0a71a6e1c195b7ee5c
Cayden Jordan <seawardspy1531@gmail.com>
2024-05-21 03:45:04 -0500

    camera impl

--- Anvil/core/src/Renderer/Systems/SpriteSystem.cpp
+++ Anvil/core/src/Renderer/Systems/SpriteSystem.cpp
@@ -12,6 +12,7 @@
 #include <Util/anvLog.hpp>
 #include <algorithm>
 #include <execution>
+#include <Renderer/Camera.h>
 
 namespace Anvil
 {
@@ -87,8 +88,8 @@ namespace Anvil
 
 		UniformBufferObject ubo{};
 		ubo.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-		ubo.view = glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-		ubo.proj = glm::perspective(glm::radians(45.0f), m_SwapChain->GetExtent().width / (float)m_SwapChain->GetExtent().height, 0.1f, 10.0f);
+		ubo.view = frameInfo.Scene->GetActiveCamera()->GetComponent<Camera>().GetViewMatrix();
+		ubo.proj = frameInfo.Scene->GetActiveCamera()->GetComponent<Camera>().GetProjectionMatrix();
 		ubo.proj[1][1] *= -1;
 		memcpy(m_UniformBuffersMapped[frameInfo.FrameIndex], &ubo, sizeof(ubo));
 	}
