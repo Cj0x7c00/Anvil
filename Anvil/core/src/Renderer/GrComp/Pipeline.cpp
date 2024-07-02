ef0b639f9acbb93027347667a90e5d5fb38aea44
Cayden Jordan <seawardspy1531@gmail.com>
2024-07-01 20:36:46 -0500

    no backface culling

--- Anvil/core/src/Renderer/GrComp/Pipeline.cpp
+++ Anvil/core/src/Renderer/GrComp/Pipeline.cpp
@@ -106,7 +106,7 @@ namespace Anvil
         rasterizer.rasterizerDiscardEnable = VK_FALSE;
         rasterizer.polygonMode = VK_POLYGON_MODE_FILL;
         rasterizer.lineWidth = 1.0f;
-        rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
+        rasterizer.cullMode = VK_CULL_MODE_NONE; /*VK_CULL_MODE_BACK_BIT*/
         rasterizer.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE;
         rasterizer.depthBiasEnable = VK_FALSE;
 
