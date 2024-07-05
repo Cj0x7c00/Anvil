c42fafa9ed36bc374c7e0d22168b4883fef9a7e2
Cayden Jordan <seawardspy1531@gmail.com>
2024-07-05 14:38:59 -0500

    fixed rendering issues

--- Anvil/core/src/Renderer/Systems/SpriteSystem.cpp
+++ Anvil/core/src/Renderer/Systems/SpriteSystem.cpp
@@ -3,12 +3,17 @@
 #include "Renderer/SwapChain.h"
 #include "Renderer/GrComp/Pipeline.h"
 
+#include "Base/anvApplication.hpp"
+
 #include "Scene/Scene.h"
+
+#include <glm/gtx/string_cast.hpp>
 
 #include <entt/entt.hpp>
 
 #include <vulkan/vulkan.h>
 #include <Util/anvLog.hpp>
+
 #include <algorithm>
 #include <execution>
 #include <Renderer/Camera.h>
@@ -37,30 +42,76 @@ namespace Anvil
 	void SpriteSystem::NewFrame(NewFrameInfo& frameInfo)
 	{
 		
-		//m_Pipeline->Bind(frameInfo.CommandBuffer.get());
-
-		//Renderer::SetViewport(ViewportInfo::Default(), frameInfo.CommandBuffer.get());
-		
 		auto& Reg = frameInfo.Scene->GetRegistry();
 		auto sprites = Reg.view<SpriteComponent, TransformComponent>();
 		auto devices = Devices::GetInstance();
-		frameInfo.CommandBuffer->BeginRecording(frameInfo, m_Pipeline);
+		frameInfo.CommandBuffer->BeginRecording();
+		Renderer::BeginRenderPass();
+		Renderer::SetViewport(ViewportInfo::Default(), frameInfo.CommandBuffer.get());
 
-		sprites.each([&](auto entity, SpriteComponent& spriteData, TransformComponent& spriteTrans)
-			{
-				(spriteData.buffersCreatedFlag == false) ? spriteData.CreatBuffers() : NULL;
-
-				update_ubos(frameInfo, spriteTrans);
-
-				Renderer::SetViewport(ViewportInfo::Default(), frameInfo.CommandBuffer.get());
-
-				spriteData.Bind(frameInfo.CommandBuffer.get(), m_Pipeline);
-				vkCmdBindDescriptorSets(frameInfo.CommandBuffer->Get(), VK_PIPELINE_BIND_POINT_GRAPHICS, 
-					m_Pipeline->GetPipelineLayout(), 0, 1, &m_DescriptorSets[frameInfo.FrameIndex], 
-					0, nullptr);
-				spriteData.Draw(frameInfo.CommandBuffer.get());
-			});
+		int count = 0;
 		
+		m_Pipeline->Bind(frameInfo.CommandBuffer.get());
+		for (auto& sprite : sprites)
+		{
+			auto [sData, tData] = sprites.get<SpriteComponent, TransformComponent>(sprite);
+
+			if (!sData.buffersCreatedFlag)
+			{
+				sData.CreateBuffers();
+				sData.buffersCreatedFlag = true;
+			}
+
+			UniformBufferObject ubo{};
+
+			ubo.model = tData.GetModelMatrix();
+			ubo.view = frameInfo.Scene->GetActiveCamera()->GetComponent<Camera>().GetViewMatrix();
+			ubo.proj = frameInfo.Scene->GetActiveCamera()->GetComponent<Camera>().GetProjectionMatrix();
+			ubo.proj[1][1] *= -1;
+			memcpy(m_UniformBuffersMapped[frameInfo.FrameIndex], &ubo, sizeof(ubo));
+
+			vkCmdBindDescriptorSets(frameInfo.CommandBuffer->Get(), VK_PIPELINE_BIND_POINT_GRAPHICS,
+				m_Pipeline->GetPipelineLayout(), 0, 1, &m_DescriptorSets[frameInfo.FrameIndex],
+			 	0, nullptr);
+
+			PushConstantData pcd{};
+			pcd.model = tData.GetModelMatrix(); //m4x4
+			pcd.view = frameInfo.Scene->GetActiveCamera()->GetComponent<Camera>().GetViewMatrix(); //m4x4
+			pcd.proj = frameInfo.Scene->GetActiveCamera()->GetComponent<Camera>().GetProjectionMatrix(); //m4x4
+			pcd.proj[1][1] *= -1;
+			pcd.color = { 0.63, 0.63, 0.63 }; // v3
+
+			//ENGINE_DEBUG("Push Constant Data:\nmodel: {}\nview: {}\nproj: {}\ncolor: {}\n", glm::to_string(pcd.model), 
+			//	glm::to_string(pcd.view), glm::to_string(pcd.proj), glm::to_string(pcd.color));
+
+			vkCmdPushConstants(frameInfo.CommandBuffer->Get(), 
+				m_Pipeline->GetPipelineLayout(), VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT, 0,
+				sizeof(PushConstantData), &pcd);
+
+			sData.Bind(frameInfo.CommandBuffer.get());
+			sData.Draw(frameInfo.CommandBuffer.get());
+			count++;
+		}
+
+		//sprites.each([&](auto entity, SpriteComponent& spriteData, TransformComponent& spriteTrans)
+		//	{
+		//		if (!spriteData.buffersCreatedFlag) {
+		//			spriteData.CreateBuffers();
+		//			spriteData.buffersCreatedFlag = true;
+		//		}
+
+		//		update_ubos(frameInfo, spriteTrans); 
+		//		vkCmdBindDescriptorSets(frameInfo.CommandBuffer->Get(), VK_PIPELINE_BIND_POINT_GRAPHICS, 
+		//			m_Pipeline->GetPipelineLayout(), 0, 1, &m_DescriptorSets[frameInfo.ImageIndex],
+		//			0, nullptr);
+
+		//		spriteData.Bind(frameInfo.CommandBuffer.get(), m_Pipeline);
+		//		spriteData.Draw(frameInfo.CommandBuffer.get());
+		//		/*count++;*/
+		//	});
+
+		//ENGINE_DEBUG("{}", count);
+		count = 0;
 	}
 
 	void SpriteSystem::create_ubos()
@@ -85,17 +136,13 @@ namespace Anvil
 
 	void SpriteSystem::update_ubos(NewFrameInfo& frameInfo, TransformComponent& trans)
 	{
-		static auto startTime = std::chrono::high_resolution_clock::now();
-
-		auto currentTime = std::chrono::high_resolution_clock::now();
-		float time = std::chrono::duration<float, std::chrono::seconds::period>(currentTime - startTime).count();
-
 		UniformBufferObject ubo{};
+
 		ubo.model = trans.GetModelMatrix();
 		ubo.view = frameInfo.Scene->GetActiveCamera()->GetComponent<Camera>().GetViewMatrix();
 		ubo.proj = frameInfo.Scene->GetActiveCamera()->GetComponent<Camera>().GetProjectionMatrix();
 		ubo.proj[1][1] *= -1;
-		memcpy(m_UniformBuffersMapped[frameInfo.FrameIndex], &ubo, sizeof(ubo));
+		memcpy(m_UniformBuffersMapped[frameInfo.ImageIndex], &ubo, sizeof(ubo));
 	}
 
 	void SpriteSystem::create_descriptor_pool()
@@ -117,19 +164,24 @@ namespace Anvil
 
 	void SpriteSystem::create_descriptor_sets()
 	{
+		ENGINE_INFO("Sprite System: Creating DST sets");
+
 		std::vector<VkDescriptorSetLayout> layouts(Renderer::MAX_FRAMES_IN_FLIGHT, m_Pipeline->GetDescriptorLayout());
+
 		VkDescriptorSetAllocateInfo allocInfo{};
 		allocInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO;
 		allocInfo.descriptorPool = m_DescriptorPool;
-		allocInfo.descriptorSetCount = static_cast<uint32_t>(Renderer::MAX_FRAMES_IN_FLIGHT);
+		allocInfo.descriptorSetCount = static_cast<uint32_t>(layouts.size());
 		allocInfo.pSetLayouts = layouts.data();
 
 		m_DescriptorSets.resize(Renderer::MAX_FRAMES_IN_FLIGHT);
+
 		if (vkAllocateDescriptorSets(Devices::GetInstance()->Device(), &allocInfo, m_DescriptorSets.data()) != VK_SUCCESS) {
 			ENGINE_WARN("failed to allocate descriptor sets");
 		}
 
-		for (size_t i = 0; i < Renderer::MAX_FRAMES_IN_FLIGHT; i++) {
+		for (size_t i = 0; i < m_DescriptorSets.size(); i++) {
+
 			VkDescriptorBufferInfo bufferInfo{};
 			bufferInfo.buffer = m_UniformBuffers[i];
 			bufferInfo.offset = 0;
@@ -137,10 +189,10 @@ namespace Anvil
 
 			VkWriteDescriptorSet descriptorWrite{};
 			descriptorWrite.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+			descriptorWrite.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;	
 			descriptorWrite.dstSet = m_DescriptorSets[i];
 			descriptorWrite.dstBinding = 0;
 			descriptorWrite.dstArrayElement = 0;
-			descriptorWrite.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;	
 			descriptorWrite.descriptorCount = 1;
 			descriptorWrite.pBufferInfo = &bufferInfo;
 
@@ -151,19 +203,95 @@ namespace Anvil
 	void SpriteSystem::load_shaders()
 	{
 		ENGINE_INFO("Sprite System: Loading Shaders");
-		std::filesystem::current_path("core/src/Renderer/Systems/");
-		auto vert = GraphicsFactory::CreateShader("Shaders/SpriteSystemV.glsl", VERTEX);
-		auto frag = GraphicsFactory::CreateShader("Shaders/SpriteSystemF.glsl", FRAGMENT);
+		
+		std::filesystem::current_path(AnvilApplication::GetLibDir().c_str());
+		std::filesystem::current_path("core/src/Renderer/Systems/Shaders/");
+
+		auto vert = GraphicsFactory::CreateShader("SpriteSystemV.glsl", VERTEX);
+		auto frag = GraphicsFactory::CreateShader("SpriteSystemF.glsl", FRAGMENT);
+
+		std::filesystem::current_path(AnvilApplication::GetWorkingDir().c_str());
 
 		m_Shaders.push_back(vert);
 		m_Shaders.push_back(frag);
-
-
 	}
 
 	void SpriteSystem::create_pipeline()
 	{
 		ENGINE_INFO("Default System: Creating Pipeline");
-		m_Pipeline = GraphicsFactory::CreatePipeline(m_Shaders);
+
+		PipelineInfo pi{};
+		pi.vertexBindingDesc = vertex::GetBindingDescription();
+		pi.vertexAttrDesc    = vertex::GetAttributeDescriptions();
+		
+		pi.vertexInputInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO,
+		pi.vertexInputInfo.vertexBindingDescriptionCount = 1;
+		pi.vertexInputInfo.vertexAttributeDescriptionCount = static_cast<uint32_t>(pi.vertexAttrDesc.size());
+		pi.vertexInputInfo.pVertexBindingDescriptions = &pi.vertexBindingDesc;
+		pi.vertexInputInfo.pVertexAttributeDescriptions = pi.vertexAttrDesc.data();
+
+		pi.asmInputInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+		pi.asmInputInfo.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+		pi.asmInputInfo.primitiveRestartEnable = VK_FALSE;
+
+		pi.viewportInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+		pi.viewportInfo.viewportCount = 1;
+		pi.viewportInfo.scissorCount = 1;
+
+		pi.rasterizerInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+		pi.rasterizerInfo.depthClampEnable = VK_FALSE;
+		pi.rasterizerInfo.rasterizerDiscardEnable = VK_FALSE;
+		pi.rasterizerInfo.polygonMode = VK_POLYGON_MODE_FILL;
+		pi.rasterizerInfo.lineWidth = 1.0f;
+		pi.rasterizerInfo.cullMode = VK_CULL_MODE_NONE;
+		pi.rasterizerInfo.frontFace = VK_FRONT_FACE_CLOCKWISE;
+		pi.rasterizerInfo.depthBiasEnable = VK_FALSE;
+
+		pi.multisampleInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+		pi.multisampleInfo.sampleShadingEnable = VK_FALSE;
+		pi.multisampleInfo.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
+
+		VkPipelineColorBlendAttachmentState colorBlendAttachment{};
+		colorBlendAttachment.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
+		colorBlendAttachment.blendEnable = VK_FALSE;
+
+		pi.colorBlendInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+		pi.colorBlendInfo.logicOpEnable = VK_FALSE;
+		pi.colorBlendInfo.logicOp = VK_LOGIC_OP_COPY;
+		pi.colorBlendInfo.attachmentCount = 1;
+		pi.colorBlendInfo.pAttachments = &colorBlendAttachment;
+		pi.colorBlendInfo.blendConstants[0] = 0.f;
+		pi.colorBlendInfo.blendConstants[1] = 0.f;
+		pi.colorBlendInfo.blendConstants[2] = 0.f;
+		pi.colorBlendInfo.blendConstants[3] = 0.f;
+
+		std::vector<VkDynamicState> dynamicStates = {
+			VK_DYNAMIC_STATE_VIEWPORT,
+			VK_DYNAMIC_STATE_SCISSOR
+		};
+
+		pi.dynamicInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO;
+		pi.dynamicInfo.dynamicStateCount = 2;
+		pi.dynamicInfo.pDynamicStates = dynamicStates.data();
+
+		// -- Push Constant Data -- //
+		VkPushConstantRange SpriteData{};
+		SpriteData.stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT;
+		SpriteData.size = sizeof(PushConstantData);
+		SpriteData.offset = 0;
+
+		pi.PushConstantRanges.push_back(SpriteData);
+
+		// -- UBO Data -- //
+		VkDescriptorSetLayoutBinding uboBinding{};
+		uboBinding.binding = 0;
+		uboBinding.descriptorCount = 1;
+		uboBinding.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+		uboBinding.pImmutableSamplers = VK_NULL_HANDLE;
+		uboBinding.stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT;
+
+		pi.DstSetLayoutBindings.push_back(uboBinding);
+
+		m_Pipeline = GraphicsFactory::CreatePipeline(m_Shaders, pi);
 	}
 }
